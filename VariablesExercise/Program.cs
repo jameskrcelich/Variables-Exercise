3a2845e6e5c4ca4a11fc186b5659910c554820ed
James Krcelich <james.krcelich@sbcglobal.net>
2025-09-06 18:30:05 -0500

    first save

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string myName = "James";
+            int principal = 5000;
+            char middleInitial = 'P';
+            bool isWorking = true;
+            double  interestRate = 4.25;
+            decimal interestRateMonthly = 354166666;
+            double  interestAmount = 0.0;
+            
+            Console.WriteLine($"My first name is {myName}, my middle initial is {middleInitial}");
+            if (isWorking == true)
+            {
+                Console.WriteLine($"I am currently employed");
+            }
+            else
+            {
+                Console.WriteLine($"I am unemployed");
+            }
+            Console.WriteLine($"My savings account is equal to ${principal}");
+            Console.WriteLine($"My yearly interest is equal to ${interestRate}%");
+            
+            interestAmount = (((double)principal) * ((double)(interestRateMonthly/100000000000)));
+            Console.WriteLine($"My first monthly interest amount is equal to ${Math.Round(interestAmount, 2)}");
+            
         }
     }
 }
